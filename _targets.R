b3928df8db57515752b4deaaed9a84ee802a07af
Andy McAliley <wmcaliley@usgs.gov>
2021-09-17 14:23:02 -0600

    Download NWIS sites as separate targets

--- _targets.R
+++ _targets.R
@@ -6,14 +6,40 @@ source("3_visualize/src/plot_timeseries.R")
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval")) # Loading tidyverse because we need dplyr, ggplot2, readr, stringr, and purrr
 
+nwis_Cd <- '00010'
 p_width <- 12
 p_height <- 7
 p_units <- "in"
 
 p1_targets_list <- list(
   tar_target(
+    site_data_01427207,
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01427207_data.csv"), parameterCd = nwis_Cd),
+    format = "file"
+  ),
+  tar_target(
+    site_data_01432160,
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01432160_data.csv"), parameterCd = nwis_Cd),
+    format = "file"
+  ),
+  tar_target(
+    site_data_01435000,
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01435000_data.csv"), parameterCd = nwis_Cd),
+    format = "file"
+  ),
+  tar_target(
+    site_data_01436690,
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01436690_data.csv"), parameterCd = nwis_Cd),
+    format = "file"
+  ),
+  tar_target(
+    site_data_01466500,
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01466500_data.csv"), parameterCd = nwis_Cd),
+    format = "file"
+  ),
+  tar_target(
     site_data,
-    download_nwis_data(),
+    nwis_df(c(site_data_01427207, site_data_01432160, site_data_01435000, site_data_01436690, site_data_01466500)),
   ),
   tar_target(
     site_info_csv,
@@ -40,7 +66,7 @@ p2_targets_list <- list(
 p3_targets_list <- list(
   tar_target(
     figure_1_png,
-    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_styled,
+    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_clean,
                          width = p_width, height = p_height, units = p_units),
     format = "file"
   )
