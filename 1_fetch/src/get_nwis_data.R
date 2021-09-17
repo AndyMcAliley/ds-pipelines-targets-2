ce522c96679698eabad563763fe83520aa591d37
Andy McAliley <wmcaliley@usgs.gov>
2021-09-17 14:11:27 -0600

    Split download and read into separate functions.

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -1,20 +1,17 @@
 
-download_nwis_data <- function(site_nums = c("01427207", "01432160", "01435000", "01436690", "01466500")){
+nwis_df <- function(site_csvs){
   
-  # create the file names that are needed for download_nwis_site_data
-  # tempdir() creates a temporary directory that is wiped out when you start a new R session; 
-  # replace tempdir() with "1_fetch/out" or another desired folder if you want to retain the download
-  download_files <- file.path(tempdir(), paste0('nwis_', site_nums, '_data.csv'))
+  # Join data in site_csvs in one data.frame
   data_out <- data.frame()
-  # loop through files to download 
-  for (download_file in download_files){
-    download_nwis_site_data(download_file, parameterCd = '00010')
+  # loop through files
+  for (site_csv in site_csvs){
     # read the downloaded data and append it to the existing data.frame
-    these_data <- read_csv(download_file, col_types = 'ccTdcc')
+    these_data <- read_csv(site_csv, col_types = 'ccTdcc')
     data_out <- bind_rows(data_out, these_data)
   }
   return(data_out)
 }
+
 
 nwis_site_info <- function(fileout, site_data){
   site_no <- unique(site_data$site_no)
