883efecebc3f7718c6810d483cbd6863f1f88aab
Andy McAliley <wmcaliley@usgs.gov>
2021-09-20 14:21:06 -0600

    Specify start and end dates in _targets file

--- _targets.R
+++ _targets.R
@@ -6,7 +6,9 @@ source("3_visualize/src/plot_timeseries.R")
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval")) # Loading tidyverse because we need dplyr, ggplot2, readr, stringr, and purrr
 
-nwis_Cd <- '00010'
+nwis_Cd <- "00010"
+start_date <- "2014-05-01"
+end_date <- "2015-05-01"
 p_width <- 12
 p_height <- 7
 p_units <- "in"
@@ -14,32 +16,51 @@ p_units <- "in"
 p1_targets_list <- list(
   tar_target(
     site_data_01427207_csv,
-    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01427207_data.csv"), parameterCd = nwis_Cd),
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01427207_data.csv"), 
+                            parameterCd = nwis_Cd,
+                            startDate = start_date,
+                            endDate = end_date),
     format = "file"
   ),
   tar_target(
     site_data_01432160_csv,
-    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01432160_data.csv"), parameterCd = nwis_Cd),
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01432160_data.csv"), 
+                            parameterCd = nwis_Cd,
+                            startDate = start_date,
+                            endDate = end_date),
     format = "file"
   ),
   tar_target(
     site_data_01435000_csv,
-    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01435000_data.csv"), parameterCd = nwis_Cd),
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01435000_data.csv"), 
+                            parameterCd = nwis_Cd,
+                            startDate = start_date,
+                            endDate = end_date),
     format = "file"
   ),
   tar_target(
     site_data_01436690_csv,
-    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01436690_data.csv"), parameterCd = nwis_Cd),
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01436690_data.csv"), 
+                            parameterCd = nwis_Cd,
+                            startDate = start_date,
+                            endDate = end_date),
     format = "file"
   ),
   tar_target(
     site_data_01466500_csv,
-    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01466500_data.csv"), parameterCd = nwis_Cd),
+    download_nwis_site_data(file.path("1_fetch", "out", "nwis_01466500_data.csv"), 
+                            parameterCd = nwis_Cd,
+                            startDate = start_date,
+                            endDate = end_date),
     format = "file"
   ),
   tar_target(
     site_data,
-    nwis_df(c(site_data_01427207_csv, site_data_01432160_csv, site_data_01435000_csv, site_data_01436690_csv, site_data_01466500_csv)),
+    nwis_df(c(site_data_01427207_csv,
+              site_data_01432160_csv,
+              site_data_01435000_csv,
+              site_data_01436690_csv,
+              site_data_01466500_csv)),
   ),
   tar_target(
     site_info_csv,
